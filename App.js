04bcaa31d5ff088e69d968f9435aa75b2869dc44
konglingxing <lingxingkong213712@sohu-inc.com>
2018-02-05 22:42:15 +0800

    add sentry

--- App.js
+++ App.js
@@ -1,3 +1,9 @@
 import App from './src/app';
 
+import { Sentry } from 'react-native-sentry';
+
+const sentryDsn = Platform.select({"ios":"https://44587f361c654dac89a556228bd0e0dd:9b16e4fe3c224d18b1ae40a4e1a69f9c@sentry.io/273686","android":"https://44587f361c654dac89a556228bd0e0dd:9b16e4fe3c224d18b1ae40a4e1a69f9c@sentry.io/273686"});
+Sentry.config(sentryDsn).install();
+
+
 export default App;
