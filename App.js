699e095e4adfc1c962e634f78cd72db601707e8d
konglingxing <lingxingkong213712@sohu-inc.com>
2018-02-05 22:51:46 +0800

    fix sentry

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
 import App from './src/app';
 
+import { Platform } from 'react-native';
+
 import { Sentry } from 'react-native-sentry';
 
 const sentryDsn = Platform.select({"ios":"https://44587f361c654dac89a556228bd0e0dd:9b16e4fe3c224d18b1ae40a4e1a69f9c@sentry.io/273686","android":"https://44587f361c654dac89a556228bd0e0dd:9b16e4fe3c224d18b1ae40a4e1a69f9c@sentry.io/273686"});
