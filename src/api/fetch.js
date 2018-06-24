ce67b2bfacf88203a02bab906b072a8a02f2d2b1
konglx <konglx90@gmail.com>
2018-06-24 12:25:40 +0800

    Update fetch.js

--- src/api/fetch.js
+++ src/api/fetch.js
@@ -25,7 +25,7 @@ function fetchRequest(url, opts={}) {
         _fetchOpts.method = opts.method
     }
     if (opts.formData) {
-        _fetchOpts.headers['Content-Type'] = 'multipart/form-data';
+        // _fetchOpts.headers['Content-Type'] = 'multipart/form-data';
         var form = new FormData();
         for (var key of opts.formData) {
             form.append(key, opts.formData[key]);
